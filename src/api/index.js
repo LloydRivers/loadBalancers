1f64a261e91031972e39907c8ded0690e2d70831
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-07-18 10:14:34 +0100

    (wip) added pg to express

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const pgp = require("pg-promise")(/* options */);
+const db = pgp(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3000;
 const INSTANCE_NAME = process.env.INSTANCE_NAME || "unknown";
@@ -13,6 +15,17 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.send(`Hello from ${INSTANCE_NAME} at ${new Date().toISOString()}`);
+});
+
+app.get("/seed", async (req, res) => {
+  try {
+    const data = await fetch("https://dummyjson.com/products");
+    const json = await data.json();
+    res.send(json);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(PORT, () => {
