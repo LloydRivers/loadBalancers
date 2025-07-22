7b15a0e79c62a8577f04090422d688bbb4dfcc57
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-07-22 11:28:09 +0100

    added alb (wip)

--- infra/terraform/load_balancer.tf
+++ infra/terraform/load_balancer.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "test" {
+resource "aws_lb" "lb" {
   name               = "lb-tf"
   internal           = false
   load_balancer_type = "application"
@@ -31,3 +31,11 @@ resource "aws_security_group" "lb_sg" {
     Name = "lb-sg"
   }
 }
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
